0b883abe4ef6295ef933101051b2d05c3c6092c7
LegeSto <55890772+LegeSto@users.noreply.github.com>
2020-07-01 21:32:50 +0300

    Add files via upload

--- file.py
+++ file.py
@@ -1,19 +1,33 @@
-from influxdb import InfluxDBClient
-import json
+from influxdb import InfluxDBClient, resultset
 import datetime
+from time import sleep
 from random import randint
 
 client = InfluxDBClient(host='localhost', port=8086)
-
 client.create_database('Table')
 print(client.get_list_database())
-json_body = [
-    {
+client.switch_database('Table')
+
+json_body = []
+
+for i in range(10):
+    s = {
         "measurement": "sec",
+        "tags": {
+            "param": i,
+            "times": datetime.datetime.now()
+        },
         "fields": {
-            "value": randint(0, 10)
+            "value": int(randint(0, 100))
         }
     }
-]
+    sleep(1)
+    json_body.append(s)
 
 client.write_points(json_body)
+results = client.query('select * from sec')
+points = results.get_points()
+
+for point in points:
+    print("Time: %s, value: %i" % (point['times'], point['value']))
+
