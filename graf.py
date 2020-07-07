c92d07df8e6fbf59c9af64b9dd3ce418f01bb130
LegeSto <55890772+LegeSto@users.noreply.github.com>
2020-07-07 16:32:52 +0300

    Изменено отображение графика

--- graf.py
+++ graf.py
@@ -1,38 +1,50 @@
-from PyQt5 import Qt
-import pyqtgraph as pg
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+from PyQt5.QtChart import QChart, QChartView, QLineSeries
+from PyQt5.QtGui import QPainter
+from PyQt5.QtCore import Qt
 from influxdb import InfluxDBClient
 
 
-class Window(Qt.QWidget):
-
+class Window(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        layout = Qt.QVBoxLayout(self)
+        self.setWindowTitle("Graphic")
+        self.setGeometry(100, 100, 700, 500)
 
-        self.view = view = pg.PlotWidget()
-        self.curve = view.plot(name="Line")
+        self.show()
 
-        self.btn = Qt.QPushButton("Plot")
-        self.btn.clicked.connect(self.plot)
+        param = '1'
+        self.create_line_chart(param)
 
-        layout.addWidget(Qt.QLabel("Graphic"))
-        layout.addWidget(self.view)
-        layout.addWidget(self.btn)
-
-    def plot(self):
+    def connect(self, param):
         client = InfluxDBClient(host='localhost', port=8086)
         client.switch_database('Table')
         results = client.query('select * from sec')
-        points = results.get_points()    # tags={'param': '1'}
-        array = []
-        for point in points:
-            array.append(point['value'])
-        self.curve.setData(array)
+        return results.get_points(tags={'param': param})
+
+    def create_line_chart(self, param):
+        series = QLineSeries()
+        for point in self.connect(param):
+            series.append(int(point['phase']), point['value'])
+
+        chart = QChart()
+
+        chart.addSeries(series)
+        chart.createDefaultAxes()
+        chart.setAnimationOptions(QChart.SeriesAnimations)
+        chart.setTitle("Parameter " + param)
+
+        chart.legend().setVisible(True)
+        chart.legend().setAlignment(Qt.AlignBottom)
+
+        chartview = QChartView(chart)
+        chartview.setRenderHint(QPainter.Antialiasing)
+
+        self.setCentralWidget(chartview)
 
 
-if __name__ == "__main__":
-    app = Qt.QApplication([])
-    w = Window()
-    w.show()
-    app.exec()
+App = QApplication(sys.argv)
+w = Window()
+sys.exit(App.exec_())
