3cda052489e35b0795617f8a054d860956b5e62a
LegeSto <55890772+LegeSto@users.noreply.github.com>
2020-07-13 16:19:27 +0300

    Add files via upload

--- rest_api.py
+++ rest_api.py
@@ -11,7 +11,10 @@ client.switch_database('Table')
 results = client.query('select * from sec')
 points = results.get_points()
 books = []
+count = 0
 for point in points:
+    point['id'] = count
+    count += 1
     books.append(point)
 
 
@@ -20,9 +23,34 @@ def home():
     return "<h1>Database InfuxDB</h1><p>This site is a prototype API.</p>"
 
 
-@app.route('/resources', methods=['GET'])
+@app.route('/resources/all', methods=['GET'])
 def api_all():
     return jsonify(books)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+
+@app.route('/resources', methods=['GET'])
+def api_filter():
+    if 'id' in request.args:
+        que = int(request.args['id'])
+        query = 'id'
+    elif 'param' in request.args:
+        que = int(request.args['param'])
+        query = 'param'
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+    result = []
+
+    for book in books:
+        if book[query] == que:
+            result.append(book)
+
+    return jsonify(result)
 
 
 app.run()
