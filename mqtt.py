ffb140d6c046f3f7cb0307f158c8ab69d36caf65
LegeSto <55890772+LegeSto@users.noreply.github.com>
2020-07-20 14:42:02 +0300

    mqtt bd update

--- mqtt.py
+++ mqtt.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 from influxdb import InfluxDBClient
 from time import sleep
+from random import randint
 
 
 class Broker(object):
@@ -16,6 +17,36 @@ def conn():
     cl = InfluxDBClient(host='localhost', port=8086)
     cl.create_database('Table')
     cl.switch_database('Table')
+
+
+def add_param():
+    print('Enter name, frequency and initial value:')
+    name, freq, value = input().split()
+    freq, value = float(freq), int(value)
+    print('Wait...')
+    p = ("Param: %s, frequency: %f, initial value: %i" % (name, freq, value))
+    phase = 0
+
+    while phase <= 10:
+        s = [{
+                "measurement": "sec",
+                "tags": {
+                    "param": name,
+                    "phase": phase,
+                    "freq": freq,
+                    "broker": br.broker_address,
+                    "topic": br.topic
+                },
+                "fields": {
+                    "value": int(value)
+                }
+            }]
+
+        value += randint(-1, 1)
+        phase += freq
+        sleep(freq)
+        cl.write_points(s)
+    return p
 
 
 def get_point():
@@ -50,6 +81,14 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("#")
 
 
+def publish_message():
+    print("Choose an action (add_param) or write a massage")
+    text = input()
+    if text.lower() == 'add_param':
+        text += add_param()
+    return text
+
+
 if __name__ == "__main__":
     conn()
     br = Broker()
@@ -64,7 +103,7 @@ if __name__ == "__main__":
     print('Connecting to broker', br.broker_address)
     client.connect(br.broker_address)
     client.loop_start()
-    client.publish(br.topic, "On")
+    client.publish(br.topic, publish_message())
     sleep(5)
     client.loop_stop()
     # client.loop_forever()
